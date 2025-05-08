032374d61a0b0f660706fd1b6e944353fc90139f
emmabloom04 <blo23006@byui.edu>
2025-05-07 19:25:46 -0600

    pushing

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -4,7 +4,7 @@ class HelloWorld extends React.Component {
    render() {
       return (
          <div>
-            <h1>Hello World!</h1>
+            <h1>Travel Doc</h1>
          </div>
       );
    }
