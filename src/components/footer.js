55ca3ea606801bf573013d40ec0e6b2bf60c0d27
emmabloom04 <blo23006@byui.edu>
2025-05-09 11:04:54 -0600

    syncing

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <p>&copy; 2025 Travel Doc.</p>
+        </footer>
+    )
+}
+
+export default Footer; 
---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li><a><img src="" alt="logo" className="logo" /></a></li>
+                    <li className="site-title">Travel Doc</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
