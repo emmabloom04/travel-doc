90f9286587b5bca2b44688dfc63f86117d6036d1
emmabloom04 <blo23006@byui.edu>
2025-05-09 10:35:57 -0600

    trying to deploy

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './components/header';
+import Content from './components/content';
+import Footer from './components/footer';
+import './styles/travel-doc.css';
+
+function App() {
+    return(
+        <div>
+            <Header />
+            <Content />
+            <Footer />
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/content.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Content() {
+    return (
+        <main>
+            <p>Welcome to your personal travel journal, Travel Doc!</p>
+        </main>
+    )
+}
+
+export default Content;
