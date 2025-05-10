0265baaaa21c6ba1d86d2d7ed248313434c2499d
emmabloom04 <blo23006@byui.edu>
2025-05-10 11:28:03 -0600

    adding logo

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+
 function Header() {
+    const logoUrl = new URL('../assets/logo.png', import.meta.url).href;
+
     return (
         <header>
             <nav>
                 <ul>
-                    <li><a><img src="" alt="logo" className="logo" /></a></li>
+                    <li><img src={logoUrl} alt="logo" className="logo" /></li>
                     <li className="site-title">Travel Doc</li>
                 </ul>
             </nav>
