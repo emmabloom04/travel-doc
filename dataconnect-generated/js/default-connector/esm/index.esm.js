66a92d61a9edb67379619e485a0e404e272e5c26
emmabloom04 <blo23006@byui.edu>
2025-06-14 16:27:16 -0600

    connecting firebase

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'travel-doc',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'travel-doc',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
